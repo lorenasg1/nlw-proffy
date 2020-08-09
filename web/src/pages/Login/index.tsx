6074c13c911b68d1f45195dc10f28bcc21f384dc
lorenafoia <lorenasg1@gmail.com>
2020-08-09 16:21:44 -0300

    Fixing login page layout

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -3,64 +3,62 @@ import React from 'react';
 import logoImg from '../../assets/images/logo.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
-import './syles.css';
+import './styles.css';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
     <div id="page-login">
-      <div id="page-login-content" className="container">
-        <div className="logo-container">
-          <img src={logoImg} alt="Proffy" />
-          <h2>
-            Sua plataforma de <br /> estudos online.
-          </h2>
-        </div>
-        <div className="form-container">
-          <div>
-            <fieldset>
-              <legend>Fazer login</legend>
+      <div className="logo-container">
+        <img src={logoImg} alt="Proffy" />
+        <h2>
+          Sua plataforma de <br /> estudos online.
+        </h2>
+      </div>
+      <div className="form-container">
+        <div>
+          <fieldset>
+            <legend>Fazer login</legend>
 
-              <form>
-                {/* <Input name="email" label="E-mail" type="email" />
+            <form>
+              {/* <Input name="email" label="E-mail" type="email" />
               <Input name="password" label="Senha" type="password" /> */}
 
-                <div className="login-input-block">
-                  <label htmlFor="email">E-mail</label>
-                  <input type="email" name="email" id="email" />
-                </div>
-                <div className="login-input-block">
-                  <label htmlFor="password">Senha</label>
-                  <input type="password" name="password" id="password" />
-                </div>
-
-                <div className="form-options">
-                  <div className="rememberme">
-                    <input id="remember" type="checkbox" />
-                    <label htmlFor="remember">Lembrar-me</label>
-                  </div>
-                  <span>
-                    <Link to="/forgot-password">Esqueci minha senha</Link>
-                  </span>
-                </div>
-
-                <button className="">Entrar</button>
-              </form>
-            </fieldset>
-
-            <footer>
-              <div>
-                <p>Não tem conta?</p>
-                <p>
-                  <Link to="/cadastro">Cadastre-se</Link>
-                </p>
+              <div className="login-input-block">
+                <label htmlFor="email">E-mail</label>
+                <input type="email" name="email" id="email" />
               </div>
-              <span>
-                É de graça {'  '}
-                <img src={purpleHeartIcon} alt="Coração roxo" />
-              </span>
-            </footer>
-          </div>
+              <div className="login-input-block">
+                <label htmlFor="password">Senha</label>
+                <input type="password" name="password" id="password" />
+              </div>
+
+              <div className="form-options">
+                <div className="rememberme">
+                  <input id="remember" type="checkbox" />
+                  <label htmlFor="remember">Lembrar-me</label>
+                </div>
+                <span>
+                  <Link to="/forgot-password">Esqueci minha senha</Link>
+                </span>
+              </div>
+
+              <button className="">Entrar</button>
+            </form>
+          </fieldset>
+
+          <footer>
+            <div>
+              <p>Não tem conta?</p>
+              <p>
+                <Link to="/cadastro">Cadastre-se</Link>
+              </p>
+            </div>
+            <span>
+              É de graça {'  '}
+              <img src={purpleHeartIcon} alt="Coração roxo" />
+            </span>
+          </footer>
         </div>
       </div>
     </div>
